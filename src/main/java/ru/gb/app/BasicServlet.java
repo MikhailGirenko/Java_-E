b10b7eb0aca2d0ae56f661586b42b8f3fef0b733
Mikhail Girenko <misha15girenko@gmail.com>
2022-12-09 12:06:40 +0300

    HomeWork1

--- src/main/java/ru/gb/app/BasicServlet.java
+++ src/main/java/ru/gb/app/BasicServlet.java
@@ -1,4 +1,4 @@
-package ru.geekbrains.servlet;
+package ru.gb.app;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,26 +12,25 @@ public class BasicServlet implements Servlet {
     // Метод вызывается контейнером после того как был создан класс сервлета
     @Override
     public void init(ServletConfig config) throws ServletException {
-    // Сохраняем полученную от сервера конфигурацию
+// Сохраняем полученную от сервера конфигурацию
         this.config = config;
     }
     @Override
     public ServletConfig getServletConfig() {
         return config;
     }
-    // Метод вызывается для каждого нового HTTP запроса к данному сервлету
     @Override
     public void service(ServletRequest req, ServletResponse res) throws
             ServletException, IOException {
-        logger.info("New request");// получаем объект типа BufferedWriter и пишем в него ответ на пришедший запрос
+        logger.info("New request");
+// получаем объект типа BufferedWriter и пишем в него ответ на пришедший запрос
         res.getWriter().println("<h1>Servlet content</h1>");
     }
     @Override
     public String getServletInfo() {
         return "BasicServlet";
     }
-    /* При завершении работы веб приложения, контейнер вызывает этот метод для
-    всех сервлетов из этого приложения*/
+
     @Override
     public void destroy() {
         logger.info("Servlet {} destroyed", getServletInfo());
